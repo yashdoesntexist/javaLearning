5df5c3d082331bb46ad5bb075852b6a609a611a5
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-17 22:34:16 -0700

    Loops Done

---
+++ LoopClass.java
@@ -0,0 +1,24 @@
+package javaLearning;
+
+public class LoopClass {
+    public static void main(String[] args) {
+        int j = 11;
+        // loops
+        // for loops
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+        // while loop
+        while (j <= 20) {
+            System.out.println(j);
+            j++;
+            
+        }
+        // do-while loop
+        int k =21;
+        do{
+            System.out.println(k);
+            k++;
+        }while(k <=30);
+    }
+}
